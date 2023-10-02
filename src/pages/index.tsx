bf5008f0dc910d766c6cac0a4a98dfce1399e002
shokishimo <shokishimo@gmail.com>
2023-10-01 20:55:54 -0500

    created Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
+import * as React from "react";
+import { Stack, Typography, Button } from "@mui/material";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Book nob</h1>
-    </div>
+    <>
+      <Head>
+        <title>FileLink Home</title>
+      </Head>
+      <Stack
+        direction="column" 
+        alignItems="center" 
+        justifyContent="center" 
+        sx={{ minHeight: "100vh" }}
+        spacing={5}
+      >
+        <Typography variant="h3">FileLink</Typography>
+        <Button size="large" variant="contained">Start</Button>
+      </Stack>
+    </>
   );
 };
 
