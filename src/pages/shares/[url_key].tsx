59278ff90d7de63d897b4770c0eaaa97c0ec5826
shokishimo <shokishimo@gmail.com>
2023-10-04 06:02:54 -0500

    zip file upload page

--- src/pages/shares/[url_key].tsx
+++ src/pages/shares/[url_key].tsx
@@ -1,19 +1,53 @@
 import type { NextPage } from "next";
 import * as React from "react";
-import { Stack } from "@mui/material";
+import { Stack, Typography, List, ListItem, Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FolderZipIcon from '@mui/icons-material/FolderZipTwoTone';
 import Head from "next/head";
-import Dropzone from "react-dropzone"
 import {useDropzone} from "react-dropzone";
+import { useState, useCallback, useEffect, useMemo } from "react";
+import { v4 as uuid } from "uuid";
 
 const SharesUrlKey: NextPage = () => {
 
-  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
-  
-  const files = acceptedFiles.map(file => (
-    <li key={file.size}>
-      {file.name}
-    </li>
-  ));
+  const [filesMap, setFilesMap] = useState(new Map<string, File>());
+
+  const addFiles = useCallback((newFiles: File[]) => {
+    setFilesMap((preMap) => {
+      const newMap = new Map(preMap);
+      newFiles.forEach(file => {
+        const ext = file.name.split('.').pop()?.toLowerCase();
+        if (ext === 'zip') {
+          newMap.set(uuid(), file);
+        }
+      });
+      return newMap;
+    });
+  }, []);
+
+  const removeFile = useCallback((uuidKey: string) => {
+    setFilesMap((prevMap) => {
+      const newMap = new Map(prevMap);
+      newMap.delete(uuidKey);
+      return newMap;
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(filesMap);
+    // show files on UI
+
+  }, [filesMap]);
+
+  const filesList = useMemo(() => Array.from(filesMap.keys()), [filesMap]);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    addFiles(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+  });
 
   return (
     <>
@@ -27,29 +61,46 @@ const SharesUrlKey: NextPage = () => {
         sx={{ minHeight: "100vh" }}
         spacing={5}
       >
-        <div style={{ borderWidth: "3px", borderStyle: "solid", borderColor: "black" }}>
-          <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-            {({getRootProps, getInputProps}) => (
-              <section>
-                <div {...getRootProps()}>
-                  <input {...getInputProps()} />
-                  <p>Drag 'n' drop some files here, or click to select files</p>
-                </div>
-              </section>
-            )}
-          </Dropzone>
+        <Typography sx={{ fontSize: "16px", color: "red" }} >
+          Zip file is only supported for now
+        </Typography>
+        <div {...getRootProps()} style={{ border: "2px dashed gray", padding: "20px", textAlign: "center", backgroundColor: "#B2BEB5", borderRadius: "10px" }}>
+          <input {...getInputProps()} />
+          {
+            isDragActive ?
+              <p>Drop zip files here ...</p> :
+              <p>Drag & drop / select zip files here</p>
+          }
         </div>
 
-        <section className="container" style={{ borderWidth: "3px", borderStyle: "solid", borderColor: "black" }}>
-          <div {...getRootProps({className: 'dropzone'})}>
-            <input {...getInputProps()} />
-            <p>Drag 'n' drop some files here, or click to select files</p>
-          </div>
-          <aside>
-            <h4>Files</h4>
-            <ul>{files}</ul>
-          </aside>
-        </section>
+        {/* Display the list of dropped files */}
+        <div>
+          <Typography sx={{ fontSize: "16px" }}>
+              Uploaded Files:
+          </Typography>
+          <List>
+            {filesList.map((key) => {
+              const file = filesMap.get(key);
+              if (!file) return null;
+
+              return (
+                <ListItem key={key} sx={{ display: "flex", alignItems: "center" }}>
+                  <FolderZipIcon color="primary" />
+                  <Typography sx={{ marginLeft: "10px", fontSize: "18px" }}>{file.name}</Typography>
+                  <Button
+                    variant="outlined"
+                    startIcon={<DeleteIcon />}
+                    onClick={() => removeFile(key)}
+                    size="small"
+                    sx={{ marginLeft: "10px", textTransform: "none", fontSize: "14px", padding: "1px 7px" }}
+                  >
+                    remove
+                  </Button>
+                </ListItem>
+              );
+            })}
+          </List>
+        </div>
       </Stack>
     </>
   );
