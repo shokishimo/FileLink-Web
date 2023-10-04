5dbfd8fc64389dd3ea64c48d909dba801f57e2d6
shokishimo <shokishimo@gmail.com>
2023-10-04 10:47:43 -0500

    added upload button

--- src/pages/shares/[url_key].tsx
+++ src/pages/shares/[url_key].tsx
@@ -2,22 +2,22 @@ import type { NextPage } from "next";
 import * as React from "react";
 import { Stack, Typography, List, ListItem, Button } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import FolderZipIcon from '@mui/icons-material/FolderZipTwoTone';
+import FolderZipIcon from "@mui/icons-material/FolderZipTwoTone";
+import FolderUploadIcon from "@mui/icons-material/DriveFolderUploadTwoTone";
 import Head from "next/head";
 import {useDropzone} from "react-dropzone";
-import { useState, useCallback, useEffect, useMemo } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { v4 as uuid } from "uuid";
 
 const SharesUrlKey: NextPage = () => {
-
   const [filesMap, setFilesMap] = useState(new Map<string, File>());
 
   const addFiles = useCallback((newFiles: File[]) => {
     setFilesMap((preMap) => {
       const newMap = new Map(preMap);
       newFiles.forEach(file => {
-        const ext = file.name.split('.').pop()?.toLowerCase();
-        if (ext === 'zip') {
+        const ext = file.name.split(".").pop()?.toLowerCase();
+        if (ext === "zip") {
           newMap.set(uuid(), file);
         }
       });
@@ -33,12 +33,6 @@ const SharesUrlKey: NextPage = () => {
     });
   }, []);
 
-  useEffect(() => {
-    console.log(filesMap);
-    // show files on UI
-
-  }, [filesMap]);
-
   const filesList = useMemo(() => Array.from(filesMap.keys()), [filesMap]);
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
@@ -48,6 +42,10 @@ const SharesUrlKey: NextPage = () => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
   });
+
+  const uploadFiles = useCallback(() => {
+    return
+  }, []);
 
   return (
     <>
@@ -61,7 +59,7 @@ const SharesUrlKey: NextPage = () => {
         sx={{ minHeight: "100vh" }}
         spacing={5}
       >
-        <Typography sx={{ fontSize: "16px", color: "red" }} >
+        <Typography sx={{ fontSize: "20px", color: "#ffae42" }} >
           Zip file is only supported for now
         </Typography>
         <div {...getRootProps()} style={{ border: "2px dashed gray", padding: "20px", textAlign: "center", backgroundColor: "#B2BEB5", borderRadius: "10px" }}>
@@ -73,11 +71,12 @@ const SharesUrlKey: NextPage = () => {
           }
         </div>
 
-        {/* Display the list of dropped files */}
         <div>
-          <Typography sx={{ fontSize: "16px" }}>
+          {filesList.length > 0 && (
+            <Typography sx={{ fontSize: "16px" }}>
               Uploaded Files:
-          </Typography>
+            </Typography>
+          )}
           <List>
             {filesList.map((key) => {
               const file = filesMap.get(key);
@@ -89,10 +88,10 @@ const SharesUrlKey: NextPage = () => {
                   <Typography sx={{ marginLeft: "10px", fontSize: "18px" }}>{file.name}</Typography>
                   <Button
                     variant="outlined"
-                    startIcon={<DeleteIcon />}
+                    startIcon={<DeleteIcon sx={{ color: "#CD5C5C" }} />}
                     onClick={() => removeFile(key)}
                     size="small"
-                    sx={{ marginLeft: "10px", textTransform: "none", fontSize: "14px", padding: "1px 7px" }}
+                    sx={{ marginLeft: "10px", textTransform: "none", fontSize: "14px", padding: "1px 7px", color: "#CD5C5C", borderColor: "#CD5C5C"}}
                   >
                     remove
                   </Button>
@@ -101,6 +100,17 @@ const SharesUrlKey: NextPage = () => {
             })}
           </List>
         </div>
+        <div style={{ marginLeft: "160px" }}>
+            <Button
+              variant="outlined"
+              size="medium"
+              startIcon={<FolderUploadIcon color="primary" />}
+              onClick={() => uploadFiles()}
+              disabled={filesList.length === 0}
+            >
+              Upload
+            </Button>
+          </div>
       </Stack>
     </>
   );
